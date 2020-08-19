46bdc020556590f65ae4d5e062f3d19c55832b0e
Martin Mederly <mederly@riverbit.cz>
2020-08-19 17:01:47 +0200

    init

---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Person.css';
+
+const person = ( props ) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name}, age {props.age}</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} />
+        </div>
+    )
+}
+
+export default person;
